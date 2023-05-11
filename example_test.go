105b3f523a3623af959ff7fad237424b52b429b8
Anton Novojilov <andy@essentialkaos.com>
2023-05-11 13:14:10 +0300

    Add usage example

---
+++ example_test.go
@@ -0,0 +1,29 @@
+package depsy
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+//                                                                                    //
+//                         Copyright (c) 2023 ESSENTIAL KAOS                          //
+//      Apache License, Version 2.0 <https://www.apache.org/licenses/LICENSE-2.0>     //
+//                                                                                    //
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+import (
+	"fmt"
+	"os"
+)
+
+// ////////////////////////////////////////////////////////////////////////////////// //
+
+func ExampleExtract() {
+	data, err := os.ReadFile("go.mod")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	deps := Extract(data, true)
+
+	for _, dep := range deps {
+		fmt.Println(dep)
+	}
+}
